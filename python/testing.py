65cdb228fa0aca256447941b22aa6c825ded97ab
Endre Vestbø <endre.vestbo@gmail.com>
2016-07-10 15:56:45 +0200

    seconds, not ms

--- python/testing.py
+++ python/testing.py
@@ -5,12 +5,12 @@ from data_read import CsvSerialReader
 from data_write import FileCsvDataWriter
 
 
-def test_reading_and_writing(port, path, execution_time_seconds):
+def test_reading_and_writing(port, path, execution_time_ms):
     start_time = time.time()
     with CsvSerialReader(port, timeout=2) as reader, \
          FileCsvDataWriter(path) as writer:
 
-        while time.time() - start_time < execution_time_seconds:
+        while time.time() - start_time < execution_time_ms:
             print('reading values')
             read_values = reader.read(3)
             print('got valuse:', read_values)
@@ -19,9 +19,10 @@ def test_reading_and_writing(port, path, execution_time_seconds):
     
 
 if __name__ == '__main__':
+    print('START')
     path, filename = os.path.split(os.path.abspath(__file__))
     path = os.path.join(path, 'testdata')
-    test_reading_and_writing('COM4', path, 30)
-    print("EXIT")
+    test_reading_and_writing('COM4', path, 15)
+    print('EXIT')
 else:
     raise SystemExit('testing.py is only a scriptfile used for testing purposes')
